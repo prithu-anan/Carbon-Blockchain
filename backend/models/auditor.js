9258224446b6951d2584967b94c27135a10d73af
Suhas Abdullah <suhasabdullah43@gmail.com>
2024-08-29 00:26:23 +0600

    added auditorSchema

--- backend/models/auditor.js
+++ backend/models/auditor.js
@@ -2,6 +2,36 @@ const mongoose = require('mongoose');
 
 const auditorSchema = new mongoose.Schema({
 
+    user: {
+        type : String 
+    } ,
+
+    walletId : {
+        type: String ,
+        required : true ,
+        unique : true 
+    } ,
+
+    role : {
+        type: String
+    } ,
+
+    firstName : {
+        type : String 
+    },
+
+    lastName : {
+        type : String 
+    } , 
+
+    email : {
+        type : String,
+        unique : true
+    },
+
+    phoneNumber : {
+        type: String
+    }
 
 });
 
